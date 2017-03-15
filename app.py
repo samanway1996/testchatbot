689433418c516d56ee0c5350e2231529c79003c5
SAMANWAY DEY <samanway1996@gmail.com>
2017-03-15 11:45:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ def makeWebhookResult(req):
     elif req.get("result").get("action") == "current.plan":
         number = parameters.get("Phonenumber")
         comp= parameters.get("Company")
-        contexts1=req.get("result").get("contexts")
-        if contexts1[0].name == "test" or contexts1[0].name=="":
-            speech = "The current plan of the user with phone no. " + number + " is " + "."
+        #contexts1=req.get("result").get("contexts")
+        #if contexts1[0].name == "test" or contexts1[0].name=="":
+        speech = "The current plan of the user with phone no. " + number + " is " + plan[number] + "."
     elif req.get("result").get("action") == "current.planchange":
         number = parameters.get("Phonenumber")
         newplan = parameters.get("Plan")
